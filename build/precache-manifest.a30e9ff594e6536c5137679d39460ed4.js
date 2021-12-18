bbbb3e9046ac96d4eac07bc1b46d83b874e43158
kulbhushanbachheti <54018662+kulbhushanbachheti@users.noreply.github.com>
2021-12-18 05:15:22 -0800

    Add files via upload

---
+++ build/precache-manifest.a30e9ff594e6536c5137679d39460ed4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d38a84e4bef5e05f68e858c56683363",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5142c48f462836cd9c62",
+    "url": "/static/js/2.23344c9c.chunk.js"
+  },
+  {
+    "revision": "c4138547da1c497e6fbae27925861f5e",
+    "url": "/static/js/2.23344c9c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37e92b08d17dc0b5d197",
+    "url": "/static/js/main.432ea19d.chunk.js"
+  },
+  {
+    "revision": "1f07c1daca2d8f3e24e4",
+    "url": "/static/js/runtime-main.fd5a71aa.js"
+  }
+]);
